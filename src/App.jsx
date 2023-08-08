92e7c3434a74f5745bf8161ec28cf5f1d8d7cc18
mattramis <121640749+mattramis@users.noreply.github.com>
2023-08-07 20:06:28 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar";
 import MainContainer from "./components/MainContainer";
 
 function App() {
